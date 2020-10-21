4d6d64992638d99eeb2c5c0a0da4d0609fb32b29
Rebecca Reid <a.rebecca.reid@gmail.com>
2020-10-20 21:18:11 -0600

    something finally works! single clicking and win states work omg. Got to figure out long press now

--- mine/msgame_user.js
+++ mine/msgame_user.js
@@ -1,12 +1,29 @@
 window.addEventListener('load', main);
 
 /**
- * creates enough cards for largest board -(9x9)- 
- * 18x14
- * registers callbacks for cards
+ * flips a single card (if coordinates valid)
  * 
  * @param {state} s 
+ * @param {number} col 
+ * @param {number} row 
  */
+function flip(s, col, row) {
+    // if (col >= 0 && col < s.getStatus().cols
+    //     && row >= 0 && row < s.getStatus().rows) {
+        s.uncover(row, col);
+    // console.log("tried to uncover" + col + "," + row);
+    // render(s);
+    // if (s.onoff == false) {
+    // s.onoff[row * s.rows + col] = !s.onoff[row * s.rows + col];
+    // }
+}
+/**
+* creates enough cards for largest board -(9x9)- 
+* 18x14
+* registers callbacks for cards
+* 
+* @param {state} s 
+*/
 function prepare_dom(s) {
     const grid = document.querySelector(".grid");
     const nCards = 18 * 14; // max grid size
@@ -17,6 +34,7 @@ function prepare_dom(s) {
         card.addEventListener("click", () => {
             card_click_cb(s, card, i);
         });
+        //long click needed
         grid.appendChild(card);
     }
 }
@@ -24,7 +42,6 @@ function prepare_dom(s) {
 /**
 * updates DOM to reflect current state
 * - hides unnecessary cards by setting their display: none
-* - adds "flipped" class to cards that were flipped
 * 
 * @param {object} s 
 */
@@ -35,16 +52,43 @@ function render(s) {
     console.log(gameRender.join("\n"));
     console.log(gameStatus);
     const grid = document.querySelector(".grid");
-    grid.style.gridTemplateColumns = `repeat(${s.cols}, 1fr)`;
+    grid.style.gridTemplateColumns = `repeat(${gameStatus.ncols}, 1fr)`;
     for (let i = 0; i < grid.children.length; i++) {
         const card = grid.children[i];
         const index = Number(card.getAttribute("data-cardInd"));
-        const currCol = index % gameStatus.ncols;
-        const currRow = index / gameStatus.ncols;
-        if (index >= s.rows * s.cols) {
-            card.style.display = "none";
+        const currCol = index % gameStatus.ncols; //is this actually ROW???
+        const currRow = Math.floor(index / gameStatus.ncols);
+        let state = "none";
+        // if (index >= s.rows * s.cols) {
+        if (index >= gameStatus.nrows * gameStatus.ncols) {
+            card.style.display = state;
         }
-
+        else {
+            state = gameRender[currRow][currCol]; //jesus? wtf is this switched?
+            console.log(state, i,"State is")
+            card.style.display = "block";
+            // console.log(state + " state of " + i);
+            //      "H" = hidden cell - no bomb
+            //      "F" = hidden cell with a mark / flag
+            //      "M" = uncovered mine (game should be over now)
+            // '0'..'9' = number of mines in adjacent cells
+            if (state === "H") {
+                card.classList.add("hidden");
+            }
+            else if (state === "F") {
+                card.classList.remove("hidden");
+                card.classList.add("flag");
+            }
+            else if (state === "M") {
+                card.classList.remove("hidden");
+                card.classList.add("exploded");
+            }
+            else {
+                card.classList.remove("hidden");
+                card.classList.add("number");
+                card.setAttribute("data-number", state); //numbers 0-9 only
+            }
+        }
         // else {
         //     card.style.display = "block";
         //     if (s.onoff[ind])
@@ -53,13 +97,10 @@ function render(s) {
         //         card.classList.remove("flipped");
         // }
     }
-    // document.querySelectorAll(".moveCount").forEach(
-    //     (e) => {
-    //         e.textContent = String(s.moves);
-    //     });
     document.querySelectorAll(".bombCount").forEach(
         (e) => {
-            e.textContent = String(gameStatus.nmines - gameStatus.nmarked);
+            e.textContent = String(gameStatus.nmarked) + " Marked / "
+                + String(gameStatus.nmines - gameStatus.nmarked) + " Total";
         });
 }
 
@@ -72,41 +113,51 @@ function render(s) {
  * @param {number} ind 
  */
 function card_click_cb(s, card_div, ind) {
-    const col = ind % s.cols;
-    const row = Math.floor(ind / s.cols);
-    card_div.classList.toggle("flipped");
+    const gameStatus = s.getStatus();
+    const col = ind % gameStatus.ncols;
+    const row = Math.floor(ind / gameStatus.ncols);
+    // card_div.classList.toggle("flipped");
     flip(s, col, row);
-    s.moves++;
+    // s.moves++;
     render(s);
+
     // check if we won and activate overlay if we did
-    if (s.onoff.reduce((res, l) => res && !l, true)) {
-      document.querySelector("#overlay").classList.toggle("active");
+    if (gameStatus.exploded === true) {
+        console.log("You lost");
+        document.querySelectorAll(".winOrLose").forEach(
+            (e) => {
+                e.textContent = String(gameStatus.nmarked) + " Marked / "
+                    + String(gameStatus.nmines - gameStatus.nmarked) + " Total";
+            });
+        document.querySelector(".winOrLose").textContent = "lose";
+
+        document.querySelector("#overlay").classList.toggle("active");
     }
-    clickSound.play();
+    // clickSound.play();
 }
 
-    
+
 function gameStartButtonClick(s, cols, rows, bombs) {
     // s.cols = cols;
     // s.rows = rows;
     // make_solvable(s);
 
     // call to msgame 
+    // I think this is swapped around, I'm getting rows and cols switched
     s.init(cols, rows, bombs);
-
     render(s);
 }
 
 function main() {
     let game = new MSGame();
-
+    game.init( 8, 10, 10);
     // get browser dimensions - not used in thise code
     let html = document.querySelector("html");
     console.log("Your render area:", html.clientWidth, "x", html.clientHeight)
 
     // register callbacks for buttons
     document.querySelectorAll(".menuButton").forEach((button) => {
-        [rows, cols, bombs] = button.getAttribute("data-size").split("x").map(s => Number(s));
+        [cols, rows, bombs] = button.getAttribute("data-size").split("x").map(s => Number(s));
         // button.innerHTML = `${cols} &#x2715; ${rows} &#x2715; ${bombs}`
         // button.addEventListener("click", button_cb.bind(null, state, cols, rows));
         button.addEventListener("click", gameStartButtonClick.bind(null, game, cols, rows, bombs));
@@ -114,11 +165,11 @@ function main() {
     });
 
     // // callback for overlay click - hide overlay and regenerate game
-    // document.querySelector("#overlay").addEventListener("click", () => {
-    //   document.querySelector("#overlay").classList.remove("active");
-    //   make_solvable(state);
-    //   render(state);
-    // });
+    document.querySelector("#overlay").addEventListener("click", () => {
+        document.querySelector("#overlay").classList.remove("active");
+        //   make_solvable(state);
+        //   render(state);
+    });
 
     // // sound callback
     // let soundButton = document.querySelector("#sound");
